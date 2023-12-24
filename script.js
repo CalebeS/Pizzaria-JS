29f527d3da0c5fd5c11b397770f4a53e71015f24
Calebe Santos <calebesantos@hotmail.com>
2023-12-24 11:07:35 -0300

    adicionando descp

--- script.js
+++ script.js
@@ -11,12 +11,19 @@ const v = (el) => document.querySelectorAll(el);
 pizzasJson.map((pizza, index) => {
     let pizzaItem = document.querySelector('.models .pizza-item').cloneNode(true);
     //preeccher as informações em pizzaItem
+    pizzaItem.setAttribute('data-key', index);
     pizzaItem.querySelector('.pizza-item--img img').src = pizza.img;
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${pizza.price.toFixed(2)}`;
     pizzaItem.querySelector('.pizza-item--name').innerHTML = pizza.name;
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = pizza.description;
     pizzaItem.querySelector('a').addEventListener('click', (e) => {
         e.preventDefault();
+        let key = e.target.closest('.pizza-item').getAttribute('data-key');
+        document.querySelector('.pizzaBig img').src = pizzasJson[key].img;
+        document.querySelector('.pizzaInfo h1').innerHTML = pizzasJson[key].name;
+        document.querySelector('.pizzaInfo--desc').innerHTML = pizzasJson[key].description;
+        document.querySelector('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzasJson[key].price.toFixed(2)}`;
+
         document.querySelector('.pizzaWindowArea').style.opacity = 0;
         document.querySelector('.pizzaWindowArea').style.display = 'flex';
         setTimeout(() => {
@@ -25,4 +32,4 @@ pizzasJson.map((pizza, index) => {
     })
     // preencher as pizzas em pizza area div.
     document.querySelector('.pizza-area').append(pizzaItem);
-})  
+})      
